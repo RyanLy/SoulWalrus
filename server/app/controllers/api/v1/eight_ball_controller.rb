891670506809095e08f290a556fd92243ac2db3a
Ryan Ly <windask@hotmail.com>
2016-04-26 03:29:00 -0400

    Eight ball table

--- server/app/controllers/api/v1/eight_ball_controller.rb
+++ server/app/controllers/api/v1/eight_ball_controller.rb
@@ -2,27 +2,14 @@ module Api::V1
   class EightBallController < ApiController
       
     def index
-      @BALL_ANSWERS = ["It is certain",
-        "It is decidedly so",
-        "Without a doubt",
-        "Yes definitely",
-        "You may rely on it",
-        "As I see it, yes",
-        "Most likely",
-        "Outlook good",
-        "Yes",
-        "Signs point to yes",
-        "Reply hazy try again",
-        "Ask again later",
-        "Better not tell you now",
-        "Cannot predict now",
-        "Concentrate and ask again",
-        "Don't count on it",
-        "My reply is no",
-        "My sources say no",
-        "Outlook not so good",
-        "Very doubtful"]
+      @resp = Aws::DynamoDB::Client.new(
+        region: 'us-east-1'
+      ).scan({
+        table_name: "eight_ball"
+      })
       
+      @BALL_ANSWERS = @resp.items.collect { |x| x['answer'] }
+
       render json: {result: @BALL_ANSWERS[rand(@BALL_ANSWERS.length)]}
      end
   end
